784b7dbdb314643748422716905c3f8ff1ae8e3b
Alexander Yakushev <alex@bytopia.org>
2020-12-08 16:43:35 +0200

    Optimize post-processing by caching demunged frames

--- src/clj_async_profiler/post_processing.clj
+++ src/clj_async_profiler/post_processing.clj
@@ -3,6 +3,7 @@
             [clojure.string :as str])
   (:import clojure.lang.Compiler
            java.io.BufferedReader
+           java.util.function.Function
            java.util.HashMap))
 
 (defn- safe-subs [^String s, ^long start, ^long end]
@@ -10,7 +11,7 @@
     (if (and (>= start 0)
              (<= end lng)
              (<= start end))
-      (.substring  s start end)
+      (.substring s start end)
       "")))
 
 (defn- frame-has-special-char?
@@ -29,58 +30,67 @@
 
 (defn demunge-java-clojure-frames
   "Transform that demunges Java and Clojure stackframes."
-  [^String s]
-  ;; Hand-rolling "regexps" here for better performance.
-  (let [lng (.length s)
-        sb (StringBuilder.)]
-    (loop [frame-beg 0]
-      (if (>= frame-beg lng)
-        (.toString sb)
-        (let [frame-end (.indexOf s ";" frame-beg)
-              frame-end (if (= frame-end -1) lng frame-end)
-              dot (.lastIndexOf s "." frame-end)
-              slash (.indexOf s "/" frame-beg)]
-          (when-not (= frame-beg 0)
-            (.append sb ";"))
-          (if (and (> slash -1) (< slash frame-end)
-                   (> dot -1) (> dot frame-beg))
-            ;; Java or Clojure frame
-            (let [^String method (.substring s (unchecked-inc-int dot) frame-end)]
-              (if (and (or (.equals method "invoke")
-                           (.equals method "doInvoke")
-                           (.equals method "invokeStatic")
-                           (.equals method "invokePrim"))
-                       ;; Exclude things like clojure/lang/Var.invoke
-                       (not (.equals "clojure/lang/" (safe-subs s frame-beg (+ frame-beg 13)))))
+  ([s] (demunge-java-clojure-frames s nil))
+  ([^String s, ^HashMap demunge-cache]
+   ;; Hand-rolling "regexps" here for better performance.
+   (let [lng (.length s)
+         sb (StringBuilder.)]
+     (loop [frame-beg 0]
+       (if (>= frame-beg lng)
+         (.toString sb)
+         (let [frame-end (.indexOf s ";" frame-beg)
+               frame-end (if (= frame-end -1) lng frame-end)
+               dot (.lastIndexOf s "." frame-end)
+               slash (.indexOf s "/" frame-beg)]
+           (when-not (= frame-beg 0)
+             (.append sb ";"))
+           (if (and (> slash -1) (< slash frame-end)
+                    (> dot -1) (> dot frame-beg))
+             ;; Java or Clojure frame
+             (let [^String method (.substring s (unchecked-inc-int dot) frame-end)]
+               (if (and (or (.equals method "invoke")
+                            (.equals method "doInvoke")
+                            (.equals method "invokeStatic")
+                            (.equals method "invokePrim"))
+                        ;; Exclude things like clojure/lang/Var.invoke
+                        (not (.equals "clojure/lang/" (safe-subs s frame-beg (+ frame-beg 13)))))
 
-                ;; Clojure frame
-                (let [^String frame (.substring s frame-beg dot)
-                      new-frame (.replace frame \/ \.)
-                      new-frame (if (frame-has-special-char? new-frame)
-                                  (Compiler/demunge new-frame)
-                                  (-> ^String new-frame
-                                      (.replace \_ \-)
-                                      (.replace \$ \/)))
-                      ;; Check if next frame has the same name, and if so, drop it.
-                      ^String possible-next-frame (.concat frame ".invokeStatic")
-                      next-frame-end (min (+ frame-end (.length possible-next-frame) 1)
-                                          lng)]
-                  (.append sb new-frame)
-                  (if (.equals possible-next-frame
-                               (safe-subs s (inc frame-end) next-frame-end))
-                    (recur (min (unchecked-inc-int next-frame-end) lng))
-                    (recur (unchecked-inc-int frame-end))))
+                 ;; Clojure frame
+                 (let [^String frame (.substring s frame-beg dot)
+                       new-frame (.replace frame \/ \.)
+                       new-frame (if (frame-has-special-char? new-frame)
+                                   (if demunge-cache
+                                     (.computeIfAbsent
+                                      demunge-cache new-frame
+                                      (reify Function
+                                        (apply [_ _]
+                                          (Compiler/demunge new-frame))))
 
-                ;; Java frame
-                (let [ ;; start (unchecked-inc-int (.lastIndexOf s ";" dot))
-                      ^String frame (.substring s frame-beg frame-end)
-                      new-frame (.replace frame \/ \.)]
-                  (.append sb new-frame)
-                  (recur (unchecked-inc-int frame-end)))))
+                                     (Compiler/demunge new-frame))
 
-            ;; Other frame
-            (do (.append sb (.substring s frame-beg frame-end))
-                (recur (unchecked-inc-int frame-end)))))))))
+                                   (-> ^String new-frame
+                                       (.replace \_ \-)
+                                       (.replace \$ \/)))
+                       ;; Check if next frame has the same name, and if so, drop it.
+                       ^String possible-next-frame (.concat frame ".invokeStatic")
+                       next-frame-end (min (+ frame-end (.length possible-next-frame) 1)
+                                           lng)]
+                   (.append sb new-frame)
+                   (if (.equals possible-next-frame
+                                (safe-subs s (inc frame-end) next-frame-end))
+                     (recur (min (unchecked-inc-int next-frame-end) lng))
+                     (recur (unchecked-inc-int frame-end))))
+
+                 ;; Java frame
+                 (let [ ;; start (unchecked-inc-int (.lastIndexOf s ";" dot))
+                       ^String frame (.substring s frame-beg frame-end)
+                       new-frame (.replace frame \/ \.)]
+                   (.append sb new-frame)
+                   (recur (unchecked-inc-int frame-end)))))
+
+             ;; Other frame
+             (do (.append sb (.substring s frame-beg frame-end))
+                 (recur (unchecked-inc-int frame-end))))))))))
 
 (defn remove-lambda-ids
   "Transform that removes numeric IDs next to anonymous functions and `eval`
@@ -94,14 +104,17 @@
   function and the default processors (demunging). Write the result to
   `out-file`. Return the total number of samples in the file."
   [stacks-file out-file transform]
-  (let [acc (HashMap.)]
+  (let [acc (HashMap.)
+        demunge-cache (HashMap.)]
     (with-open [f (io/reader stacks-file)]
       (loop []
         (when-let [line (.readLine ^BufferedReader f)]
           (let [sep (.lastIndexOf line " ")
                 stack (subs line 0 sep)
                 count (Long. (subs line (inc sep)))
-                xstack (-> stack demunge-java-clojure-frames transform)]
+                xstack (-> stack
+                           (demunge-java-clojure-frames demunge-cache)
+                           transform)]
             (when xstack
               (let [value (get acc xstack 0)]
                 (.put acc xstack (+ value count))))
