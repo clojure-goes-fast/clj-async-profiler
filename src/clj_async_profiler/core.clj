3398a2386bae5bb7d5a7c57f8619d69891f62a1c
Alexander Yakushev <alex@bytopia.org>
2021-04-13 22:23:10 +0300

    Improve way of obtaining self PID

--- src/clj_async_profiler/core.clj
+++ src/clj_async_profiler/core.clj
@@ -152,11 +152,29 @@
         (throw (ex-info (str "File " f " does not exist.") {})))
       {:id (if id (Integer/parseInt id) -1), :stacks-file f, :event (keyword (or event :cpu))})))
 
+(defmacro ^:private get-self-pid*
+  "This macro expands into proper way of obtaining self PID on JDK9+, and digs
+  into internals on JDK8."
+  []
+  (if (and (try (resolve 'java.lang.ProcessHandle)
+                (catch ClassNotFoundException _))
+           ;; (java.lang.ProcessHandle/current) doesn't work before 1.10.0
+           ;; because of a Clojure bug.
+           (>= (compare (mapv *clojure-version* [:major :minor]) [1 10]) 0))
+    `(.pid (java.lang.ProcessHandle/current))
+    `(let [runtime# (ManagementFactory/getRuntimeMXBean)
+           jvm# (.get (doto (.getDeclaredField (class runtime#) "jvm")
+                        (.setAccessible true))
+                      runtime#)]
+       (.invoke (doto (.getDeclaredMethod (class jvm#) "getProcessId"
+                                          (into-array Class []))
+                  (.setAccessible true))
+                jvm# (object-array [])))))
+
 (defn get-self-pid
   "Returns the process ID of the current JVM process."
   []
-  (let [^String rt-name (.getName (ManagementFactory/getRuntimeMXBean))]
-    (subs rt-name 0 (.indexOf rt-name "@"))))
+  (get-self-pid*))
 
 (defn- make-command-string
   [command {:keys [file event interval framebuf threads]
