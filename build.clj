d0b493d2a294b4730a1e416037fcfa4ff19ad9a9
Oleksandr Yakushev <alex@bytopia.org>
2024-09-09 20:24:53 +0300

    [ci] Set up autodeploys

--- build.clj
+++ build.clj
@@ -4,9 +4,8 @@
             [clojure.tools.build.tasks.write-pom]
             [deps-deploy.deps-deploy :as dd]))
 
-(def default-opts
-  (let [url "https://github.com/clojure-goes-fast/clj-async-profiler"
-        version "1.2.2"]
+(defn default-opts [version]
+  (let [url "https://github.com/clojure-goes-fast/clj-async-profiler"]
     {;; Pom section
      :lib 'com.clojure-goes-fast/clj-async-profiler
      :version version
@@ -24,9 +23,11 @@
      :class-dir "target/classes"}))
 
 (defmacro opts+ [& body]
-  `(let [~'opts (merge default-opts ~'opts)]
+  `(let [~'opts (merge (default-opts (:version ~'opts)) ~'opts)]
      ~@body
      ~'opts))
+
+(defn log [fmt & args] (println (apply format fmt args)))
 
 (defn- jar-file [{:keys [target lib version]}]
   (format "%s/%s-%s.jar" target (name lib) version))
@@ -61,14 +62,24 @@
       (b/copy-dir {:src-dirs   (:paths basis)
                    :target-dir class-dir
                    :include    "**"
-                   :ignores    [#".+\.java"]})
+                   :ignores    [#_#".+\.java"]})
       (b/jar (assoc opts :jar-file jar)))))
 
-(defn deploy "Deploy the JAR to Clojars." [opts]
+(defn deploy "Deploy the JAR to Clojars." [{:keys [version] :as opts}]
+  (assert (and version (re-matches #"\d+\.\d+\.\d+.*" version)))
   (opts+
-    (dd/deploy {:installer :remote
-                :artifact (b/resolve-path (jar-file opts))
-                :pom-file (b/pom-path opts)})))
+   (jar opts)
+   (log "Deploying %s to Clojars..." version)
+   (dd/deploy {:installer :remote
+               :artifact (b/resolve-path (jar-file opts))
+               :pom-file (b/pom-path opts)})))
+
+(defn install [opts]
+  (let [opts (update opts :version #(or % "99.99"))]
+   (opts+
+    (jar opts)
+    (log "Installing %s to local Maven repository..." (:version opts))
+    (b/install (assoc opts :jar-file (jar-file opts))))))
 
 ;; To recompile Java class at runtime:
 ;; ((requiring-resolve 'virgil/compile-java) ["src"])
