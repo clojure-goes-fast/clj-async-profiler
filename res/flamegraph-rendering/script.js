52db8d3e7e96c3b25040cd599c825d2f24d2f64b
Nikita Prokopov <niki@tonsky.me>
2024-11-21 12:49:08 +0100

    Make buttons’ padding is smallbar bigger

--- res/flamegraph-rendering/script.js
+++ res/flamegraph-rendering/script.js
@@ -80,7 +80,7 @@ function updateSidebarState() {
     canvasWidth = availableWidth - sidebarWidth;
   } else {
     sidebar.style.display = 'none';
-    smallbar.style.display = 'block';
+    smallbar.style.display = 'flex';
     canvasWidth = availableWidth;
   }
   canvasWidth -= 20; // Reduce by the padding of .graphCol
